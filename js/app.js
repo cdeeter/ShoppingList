b18e84b5121e95eb61886611a0328c1f63487f6a
cdeeter <celiadeeter@celiadesmacbook.attlocal.net>
2014-08-15 13:13:18 -0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+function add(itementer, list) {
+
+	var delete_item = "<div id='x'>x</div>"
+
+	$("#"+list).prepend("<li>"+$('input[id='+itementer+']').val()+"</li>").prepend("<div id='x'>x</div>");
+	$('#itementer').val('');
+
+}
+
+
+$(document).ready(function() {
+
+
+	$("#list").on("click", "li", function() {
+		$(this).toggleClass("click");
+	});
+
+	$("#list").on("click", "#x", function(){
+		$(this).next("li").remove();
+		$(this).remove();
+	});
+
+});
