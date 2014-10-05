33969f8512451067b9ab88bc5d9cbc2f2d683089
cdeeter <celiadeeter@celiadesmacbook.attlocal.net>
2014-10-05 11:33:11 -0700

    css color edit, added comments to JS

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+//add items to list
 function add(itementer, list) {
 
 	var delete_item = "<div id='x'>x</div>"
@@ -11,21 +11,23 @@ function add(itementer, list) {
 
 $(document).ready(function() {
 
-
+    //change color of selected item
 	$("#list").on("click", "li", function() {
 		$(this).toggleClass("click");
 	});
-
+    
+    //delete items
 	$("#list").on("click", "#x", function(){
 		$(this).next("li").remove();
 		$(this).remove();
 	});
 
-
+    //clear list
 	$("#removeall").click(function() {
 		$("#list").empty();
 	});
 
+    //add item on enter press
 	$("#itementer").keyup(function(event){
 		if(event.keyCode == 13) {
 			$("#addbutton").click();
